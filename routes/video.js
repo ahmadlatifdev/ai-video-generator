8d9338e4ef5df6e7cb9d6fa9ae91fb931e57e944
ahmadlatifdev <privategate777@gmail.com>
2025-05-02 12:34:23 -0400

    Update video.js

--- routes/video.js
+++ routes/video.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  res.json({ message: 'Video generated successfully!' });
+});
+
+module.exports = router;
 
